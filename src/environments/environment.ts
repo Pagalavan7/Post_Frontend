9b61235fa5f3d7739a7b311192b1a906d385d2f3
Pagalavan7 <pagalavanvetri@gmail.com>
2024-10-22 19:31:20 +0530

    env file update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false, // Indicates the app is in development mode
-  apiBaseUrl: 'https://post-backend-qepi.onrender.com', // Local backend API URL for development
+  apiBaseUrl: 'http://localhost:3000/api', // Local backend API URL for development
   enableDebug: true, // Enable debug information (logging, error traces)
 };
