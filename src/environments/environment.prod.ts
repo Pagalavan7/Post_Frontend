ffeb6b9474857a526ac311376bdbc6e8b2bf8b95
Pagalavan7 <pagalavanvetri@gmail.com>
2024-10-22 19:39:56 +0530

    prod link add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true, // Indicates the app is in production mode
-  apiBaseUrl: 'http://localhost:3000/api', // Secure backend API URL for production
+  apiBaseUrl: 'https://post-backend-qepi.onrender.com', // Secure backend API URL for production
   enableDebug: false, // Disable debug information for security
   contentSecurityPolicy:
     "default-src 'self'; script-src 'self'; style-src 'self' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;",
